f88dc1fa4e7fd1c3d5ae8a60016c1106b34ae095
Arsa Fauzi <arsafauzi@gmail.com>
2026-03-09 14:48:28 +0700

    RE:update changes for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [
     react(),
+    {
+      name: 'add-nojekyll',
+      closeBundle() {
+        writeFileSync('dist/.nojekyll', '')
+      }
+    }
   ],
   base: '/',
 })
